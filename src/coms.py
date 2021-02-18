f078eea2f41d713819a56b8d6d11d8d303e5b4bc
scallait <sebastiencallait@gmail.com>
2021-02-18 11:53:57 -0800

    Added configs, xbee_callback attributes, installed attr library, added important and install directions to requirements.txt

--- src/coms.py
+++ src/coms.py
@@ -1,28 +1,37 @@
-# TODO: add import statements
+import attr
+# TODO:add import statements
 
+
+@attr.s                                                       #Defines class as an attr type class
 class Coms():
   '''Compartmentalizes coms functionality and scope'''
-  
-  def __init__(self):
-    # TODO: implement
+  configs = attr.ib()
+  xbee_callback = attr.ib()
 
   def send_till_ack(self):
     # TODO: implement
+    pass
 
   def acknowledge(self):
     # TODO: implement
+    pass
 
   def new_msg_id(self):
     # TODO: implement
+    pass
 
   def receive(self):
     # TODO: implement
+    pass
 
   def send(self):
     # TODO: implement
+    pass
 
   def bad_msg(self):
     # TODO: implement
+    pass
 
   def comm_simulation(self):
     # TODO: implement
+    pass
